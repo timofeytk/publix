a7a9cbc8bcb2c2cf051c054e9f011f141875a346
timofeytk <timofeytk@yandex.ru>
2022-11-16 22:00:10 +0600

    updated 16-12

--- script.js
+++
@@ -1,7 +0,0 @@
-// let circle = document.querySelector('.publix-wrapper');
-// let winHeight = window.outerHeight;
-// let winWidth = window.outerWidth;
-
-
-// circle.style.width = winHeight - 250 + 'px';
-// circle.style.height = winHeight - 250 + 'px';
